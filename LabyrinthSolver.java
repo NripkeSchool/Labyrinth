e9f68ca0d386e4a02a956b014d3a6eb749d5395c
NripkeSchool <89931379+NripkeSchool@users.noreply.github.com>
2021-11-22 15:31:24 -0500

    Finished Labyrinth Solver

--- LabyrinthSolver.java
+++ LabyrinthSolver.java
@@ -4,7 +4,6 @@ public class LabyrinthSolver
 {
     private ArrayList<Integer> movesX;
     private ArrayList<Integer> movesY;
-    private ArrayList<int[]> solution;
     private int maxRow;
     private int maxCol;
     private Labyrinth maze;
@@ -33,7 +32,6 @@ public class LabyrinthSolver
         
         movesX = new ArrayList<Integer>();
         movesY = new ArrayList<Integer>();
-        solution = new ArrayList<int[]>();
         
         movesX.add(0);
         movesY.add(0);
@@ -46,57 +44,56 @@ public class LabyrinthSolver
     
     public int[] solveArray()
     {
-        int[] newSolution = new int[solution.size()];
+        int[] solution = new int[movesX.size()-1];
+        //Up = 0, Down = 1, Left = 2, Right = 3
         
-        for (int i = 0; i<solution.size(); i++)
+        for (int i = 0; i<movesX.size()-1; i++)
         {
-            if (solution.get(i) == Labyrinth.UP)
-          {
-              newSolution[i] = 0;
-          }
-          if (solution.get(i) == Labyrinth.DOWN)
-          {
-              newSolution[i] = 1;
-          }
-          if (solution.get(i) == Labyrinth.LEFT)
-          {
-              newSolution[i] = 2;
-          }
-          if (solution.get(i) == Labyrinth.RIGHT)
-          {
-              newSolution[i] = 3;
-          }
+            int xDif = movesX.get(i+1) - movesX.get(i);
+            int yDif = movesY.get(i+1) - movesY.get(i);
+            if (xDif != 0) //If it went Up/Down
+            {
+                if (xDif == 1) //Down
+                {
+                    solution[i] = 1;
+                    System.out.println("DOWN");
+                }else { //UP
+                    solution[i] = 0;
+                    System.out.println("UP");
+                }
+            }else { //If it went up/down
+                if (yDif == 1) //Right
+                {
+                    solution[i] = 3;
+                    System.out.println("RIGHT");
+                }else if (yDif == -1) { //Left
+                    solution[i] = 0;
+                    System.out.println("LEFT");
+                }
+            }
         }
-        
-        return newSolution;
+        return solution;
     }
     
     public void printSolution()
     {
-        for (int i = 0; i<solution.size(); i++)
+        for (int row = 0; row<maxRow; row++)
         {
-          if (solution.get(i) == Labyrinth.UP)
-          {
-              System.out.println("UP");
-          }
-          if (solution.get(i) == Labyrinth.DOWN)
-          {
-              System.out.println("DOWN");
-          }
-          if (solution.get(i) == Labyrinth.LEFT)
-          {
-              System.out.println("LEFT");
-          }
-          if (solution.get(i) == Labyrinth.RIGHT)
-          {
-              System.out.println("RIGHT");
-          }
+            for (int col = 0; col<maxCol; col++)
+            {
+                if (maze.isStone(row, col))
+                {
+                   System.out.print("_ "); 
+                }else {
+                   System.out.print("# ");  
+                }
+            }
+            System.out.println();
         }
     }
     
     public boolean findSafeMove(int row, int col)
     {
-        System.out.println("Row/Col: " + row + ", " + col);
         if (row==(maxRow-1) && col==(maxCol-1))
         {
             System.out.println("Solved");
@@ -110,7 +107,6 @@ public class LabyrinthSolver
             makeMove(row-1, col);
             if(findSafeMove(row-1, col))
             {
-                solution.add(maze.UP);
                 return true;
             }
             removeMove(row-1, col);
@@ -122,7 +118,6 @@ public class LabyrinthSolver
             makeMove(row+1, col);
             if (findSafeMove(row+1, col))
             {
-                solution.add(maze.DOWN);
                 return true;
             }
             removeMove(row+1, col);
@@ -134,7 +129,6 @@ public class LabyrinthSolver
             makeMove(row, col-1);
             if(findSafeMove(row, col-1))
             {
-                solution.add(maze.LEFT);
                 return true;
             }
             removeMove(row, col-1);
@@ -146,7 +140,6 @@ public class LabyrinthSolver
             makeMove(row, col+1);
             if (findSafeMove(row, col+1))
             {
-                solution.add(maze.RIGHT);
                 return true;
             }
             removeMove(row, col+1);
