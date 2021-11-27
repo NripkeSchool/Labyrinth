7849627702feffe2853c0fb25cd057a0c2810462
NripkeSchool <89931379+NripkeSchool@users.noreply.github.com>
2021-11-26 22:36:26 -0500

    Update LabyrinthSolver.java

--- LabyrinthSolver.java
+++ LabyrinthSolver.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 /**
- * The LabyrinthSolver class is used to solve
+ * The LabyrinthSolver class is used to solve an instance of the Labyrinth class
+ * which creates a random maze. It utilizes a recursive method to traverse the maze 
+ * called findSafeMove() which returns true whenever it reaches the end. 
  */
 
 public class LabyrinthSolver
 {
-    private ArrayList<Integer> movesX;
-    private ArrayList<Integer> movesY;
+    private ArrayList<Integer> movesX; //Contains row position of moves
+    private ArrayList<Integer> movesY; //Contains column position of moves
     private int maxRow;
     private int maxCol;
     private Labyrinth maze;
@@ -17,19 +19,26 @@ public class LabyrinthSolver
         int row = Integer.parseInt(args[0]);
         int col = Integer.parseInt(args[1]);
         
-        Labyrinth l = new Labyrinth(row, col);
-        LabyrinthSolver ls = new LabyrinthSolver(row, col, l);  
+        LabyrinthSolver ls = new LabyrinthSolver(row, col);  
         
         if(ls.findSafeMove(0, 0))
         {
            ls.printSolution();  
         }
         
-        System.out.println(ls.getMaze().solves(ls.solves(l)));
+        System.out.println(ls.getMaze().solves(ls.solves(ls.getMaze())));
     }
     
-    public LabyrinthSolver(int row, int col, Labyrinth l)
+    
+    /**
+     * Constructor for LabyrinthSolver
+     * @return void
+     * @param Takes an integer representing the # of rows in the maze
+     * @param Takes an integer representing the # of columns in the maze
+     */
+    public LabyrinthSolver(int row, int col)
     {
+        Labyrinth l = new Labyrinth(row, col);
         maze = l;
         
         maxRow = row;
@@ -38,13 +47,20 @@ public class LabyrinthSolver
         movesX = new ArrayList<Integer>();
         movesY = new ArrayList<Integer>();
         
+        //The first move is always the starting square
         movesX.add(0);
         movesY.add(0);
     }
     
+    /**
+     * The solves method outputs a list of instructions to complete the maze which
+     * can be fed to the Labyrinth class to check if it actually solves the maze.
+     * @return Array of integers representing the direction of the move (Up = 0, Down = 1, Left = 2, Right = 3)
+     * @param Takes a Labyrinth
+     */
     public int[] solves(Labyrinth l)
     {
-        int[] solution = new int[movesX.size()-1];
+        int[] solution = new int[movesX.size()-1]; //1 Less because it doesn't contain starting square
         //Up = 0, Down = 1, Left = 2, Right = 3
         
         for (int i = 0; i<movesX.size()-1; i++)
@@ -59,7 +75,7 @@ public class LabyrinthSolver
                 }else { //UP
                     solution[i] = 0;
                 }
-            }else { //If it went up/down
+            }else { //If it went Left/Right
                 if (yDif == 1) //Right
                 {
                     solution[i] = 3;
@@ -71,11 +87,18 @@ public class LabyrinthSolver
         return solution;
     }
     
+    /**
+     * Useful getter method
+     * @return A Labyrinth Object representing the current maze
+     */
     public Labyrinth getMaze()
     {
         return maze;
     }
 
+    /**
+     * 
+     */
     public void printSolution()
     {
         for (int row = 0; row<maxRow; row++)
@@ -93,11 +116,18 @@ public class LabyrinthSolver
         }
     }
     
+    /**
+     * The all-imporant recursive method which actually traverses the maze. It also
+     * updates the movesX and movesY arrays so that they contain all the correct moves once finished
+     * @return Boolean if it reached the end or not
+     * @param An integer representing the current row of the maze
+     * @param An integer representing the current column of the maze
+     */
     public boolean findSafeMove(int row, int col)
     {
         if (row==(maxRow-1) && col==(maxCol-1))
         {
-            return true;
+            return true; //Reached the end
         }
         
         //Up
@@ -147,11 +177,25 @@ public class LabyrinthSolver
         return false;
     }
     
+    /**
+     * The isSafe() method will check whether or not the location fed to it is a viable square to move to
+     * for the findSafeMove() method.
+     * @return Boolean if the square is in the maze/we haven't gone there/it is stone
+     * @param An integer representing the current row position we are checking
+     * @param An integer representing the current column position we are checking
+     */
     public boolean isSafe(int row, int col)
     {
        return maze.isValid(row, col) && maze.isStone(row, col)&& hasntMoved(row, col);
     }
     
+    /**
+     * Useful method that searches through the current row and column moves to see if
+     * a move has already been made
+     * @return Boolean if we have moved there are not
+     * @param An integer representing the current row position we are checking
+     * @param An integer representing the current column position we are checking
+     */
     public boolean hasntMoved(int row, int col)
     {
         for (int i = 0; i<movesX.size(); i++)
@@ -165,12 +209,24 @@ public class LabyrinthSolver
         return true; //They haven't moved here
     }
     
+    /**
+     * Method for simplification that simply adds a move location to our moves arrays
+     * @return void
+     * @param An integer representing the row to add
+     * @param An integer representing the column to add
+     */
     public void makeMove(int row, int col)
     {
         movesX.add(row);
         movesY.add(col);
     }
     
+    /**
+     * Method for simplification that removes a move from our array
+     * @return void
+     * @param An integer representing the row to remove
+     * @param An integer representing the column to remove
+     */
     public void removeMove(int row, int col)
     {
         for (int i = 0; i<movesX.size(); i++)
